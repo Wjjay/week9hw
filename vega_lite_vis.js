eb97bea3570587e4262e47bb1976088fdaae41ce
Wjjay <91184540+Wjjay@users.noreply.github.com>
2021-09-24 12:34:52 +1000

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,4 +1,4 @@
-var vg_1 = "https://raw.githubusercontent.com/Wjjay/week9hw/main/co2_by_country.json"; // Looks like it can't load this file for some reason.
+var vg_1 = "co2_by_country.json"; // Looks like it can't load this file for some reason.
 vegaEmbed("#content", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
